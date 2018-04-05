4c0cb386489d667c4225596ccc19a928a726af37
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-04-05 17:54:09 -0300

    projeto-fatec

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 
 require('./config/config');
 
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static('C:/trabalho-web-fatec' + '/public'))
 require('./routes/routes')(app);
 
 app.listen(global.config.port, () => {
