195fa86672d703eb068888d443bdb08ccfd77d45
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-04-05 19:51:16 -0300

    colaboradores

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
 const home =  require('../controllers/index');
 const memes = require('../controllers/memes');
+const colaboradores = require('../controllers/colaboradores');
 
 module.exports = (app) => {
     app.get('/', home.exibir);
     
     app.get('/memes', memes.selecionar);
+
+    app.get('/colaboradores', colaboradores.exibir);
 };
